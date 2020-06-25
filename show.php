2f97c9c4ae80ca54cb4947033029fee34fbf0d18
belcaid abderrahim <belcaidna@gmail.com>
2020-06-25 12:30:28 +0100

    version 1.2

--- show.php
+++ show.php
@@ -1,5 +1,5 @@
 <?php
-
+//absolute path to the WordPress directory.
 require_once(ABSPATH . 'wp-config.php');
   $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
   mysqli_select_db($connection, DB_NAME);
@@ -9,16 +9,19 @@ require_once(ABSPATH . 'wp-config.php');
 $sql = "SELECT * from FistPlugins";
 if($result = mysqli_query($connection, $sql)){
     if(mysqli_num_rows($result) > 0){
-        echo '<table class="form-table">';
-            echo "<tr>";
-                
-                echo "<th>First name</th>";
-                echo "<th>Last name</th>";
-                echo "<th>Password</th>";
-                echo "<th>Description</th>";
-                echo "<th>Option</th>";
+        echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+        echo '<table class="table table-bordered" style="width: 99%;margin-top: 32px;">';
+            echo '<thead class="thead-dark">';
+        echo '<tr>';
+                echo '<th scope="col">First name</th>';
+                echo '<th scope="col">Last name</th>';
+                echo '<th scope="col">Password</th>';
+                echo '<th scope="col">Description</th>';
+                echo '<th scope="col">Option</th>';
             echo "</tr>";
+        echo "</thead>";
         while($row = mysqli_fetch_array($result)){
+            echo "<tbody>";
             echo "<tr>";
                 echo "<td>" . $row['username'] . "</td>";
                 echo "<td>" . $row['fname'] . "</td>";
@@ -26,8 +29,11 @@ if($result = mysqli_query($connection, $sql)){
                 echo "<td>" . $row['descriptions'] . "</td>";
                 echo "<td>" . $row['Options'] . "</td>";
             echo "</tr>";
+            echo "</tbody>";
+            echo "</tbody>";
         }
         echo "</table>";
+        echo '<a class="btn btn-primary" href="admin.php?page=PLUGIN_SUBMENU2" role="button">ADD</a>';
     }
 
 
